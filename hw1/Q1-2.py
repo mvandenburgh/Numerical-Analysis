c615fca5cae7617c8ea9282674db29768143d288
Michael VanDenburgh <mfvandenburg@cs.stonybrook.edu>
2020-02-10 16:31:26 -0500

    Started q1.2

---
+++ hw1/Q1-2.py
@@ -0,0 +1,29 @@
+from math import cos
+
+# Function that performs the bisection method. 
+# Source: https://www.math.ubc.ca/~pwalls/math-python/roots-optimization/bisection/
+def bisection(f,a,b,N):
+    if f(a)*f(b) >= 0:
+        print("Bisection method fails.")
+        return None
+    a_n = a
+    b_n = b
+    for n in range(1,N+1):
+        m_n = (a_n + b_n)/2
+        f_m_n = f(m_n)
+        if f(a_n)*f_m_n < 0:
+            a_n = a_n
+            b_n = m_n
+        elif f(b_n)*f_m_n < 0:
+            a_n = m_n
+            b_n = b_n
+        elif f_m_n == 0:
+            print("Found exact solution.")
+            return m_n
+        else:
+            print("Bisection method fails.")
+            return None
+    return (a_n + b_n)/2
+
+f = lambda x: 2.020**(-x**3)-(x**3)*(cos(x**4))-1.984
+print(bisection(f, -0.95, -0.55, 1000))
