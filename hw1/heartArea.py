7d1acf052fa2c401446b82918d64d7ffc20dd1fb
Michael VanDenburgh <mfvandenburg@cs.stonybrook.edu>
2020-02-12 01:33:36 -0500

    implemented custom integration algo, removed scipy

--- hw1/heartArea.py
+++ hw1/heartArea.py
@@ -3,21 +3,46 @@
 ##################################################
 
 from math import sqrt
-from scipy import integrate # TODO: remove this
 from time import time
 startTime = time()
 
-# TODO: Implement my own integration algorithm
 
-# equation of the top part of the heart on the left side of the y-axis
+def integrate(f, a, b, n=10000):
+    """ Computes the definite integral of the function f from a to b using Simpson's Rule. 
+    Optional argument n to provide number of subdivisions (default 10000)."""
+    deltaX = (b-a) / n
+    sum = 0
+    for i in range(n):
+        x = a + i * deltaX
+        if i == 0:
+            try:
+                sum += f(x)
+            except:
+                pass
+        elif i % 2 == 1:
+            try: 
+                sum += (4 * f(x))
+            except:
+                pass
+        else:
+            try: 
+                sum += (2 * f(x))
+            except:
+                pass
+    sum += f(a + n * deltaX)
+    return (deltaX / 3) * sum
+
+
 def topLeft(x):
+    """ Equation of the top part of the heart on the left side of the y-axis. """
     return sqrt(-x) + sqrt(-(x**2) - x - abs(x) + 2)
 
-# equation of the bottom part of the heart on the left side of the y-axis
 def bottomLeft(x):
+    """ Equation of the bottom part of the heart on the left side of the y-axis. """
     return sqrt(-x) - sqrt(-(x**2) - x - abs(x) + 2)
 
-area = 2 * (integrate.quad(topLeft, -sqrt(2), 0)[0] - integrate.quad(bottomLeft, -sqrt(2), -1)[0] + integrate.quad(bottomLeft, 0, -1)[0])
-print("Area of heart: " + str(area))
+
+area = 2 * (integrate(topLeft, -sqrt(2), 0) - integrate(bottomLeft, -sqrt(2), -1) + integrate(bottomLeft, 0, -1))
+print("Area of heart = " + str(area))
 
 print("Program finished in " + str(time()-startTime) + " seconds")
