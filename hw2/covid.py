1450221610947b69321267b63b8eaab794d1f801
Michael VanDenburgh <mfvandenburg@cs.stonybrook.edu>
2020-03-16 10:26:13 -0400

    implement custom rng

--- hw2/covid.py
+++ hw2/covid.py
@@ -1,19 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from random import random
 from numpy import sin, cos, sqrt, log, pi
+from math import fmod
+from random import random
 
-def genNumbers(count):
-    vals = []
-    for i in range(0, count):
-        vals.append(random())
-    return vals
+x0 = 0.375205
+x1 = 0.376047
+def rng():
+    global x0
+    global x1
+
+    temp = x0
+    x0 = x1
+    if x1 + temp > 1.0:
+        x1 = fmod((x1 + temp) - 1.0, 1.0)    
+    else:
+        x1 = fmod((x1 + temp), 1.0)
+    return x1
 
 
 def box_muller_transform(mu, sigma):
     
-    x1 = random()
-    x2 = random()
+    x1 = rng()
+    x2 = rng()
     
     g1 = sqrt(-2 * log(x1)) * cos(2 * pi * x2)
     g2 = sqrt(-2 * log(x1)) * sin(2 * pi * x2)
@@ -25,8 +34,8 @@ def box_muller_transform(mu, sigma):
 
 dist = []
 
-for i in range(0, 1000000):
+for i in range(0, 100000):
     dist.append(box_muller_transform(0.18, sqrt(0.08))[0])
 
-plt.hist(dist, 100, density=True)
+plt.hist(dist, 1000, density=True)
 plt.show()
