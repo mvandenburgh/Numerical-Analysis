be386b5b4683bd53dc077931bb6b7221c726e712
Michael VanDenburgh <mfvandenburg@cs.stonybrook.edu>
2020-04-24 14:30:56 -0400

    use double instead of float

--- hw3/Q2-MonteCarlo.c
+++ hw3/Q2-MonteCarlo.c
@@ -6,32 +6,48 @@
 
 #define D 0.75 // diameter of disc
 #define W 1 // distance between parallel lines
-#define SAMPLES 1984444444 // number of discs to drop
+#define SAMPLES 1984444444 // number of discs to toss
 
-#if SAMPLES < 100000
+#if SAMPLES <= 100000
 #define GRAPH // define GRAPH to display graph of data
 /** NOTE - due to the use of a file buffer for storing the data for graphing,
  *  large sample sizes will not be graphed to prevent filling up of disc space
  */
 #endif
 
+
+// Seeds for pseudo-RNG 
+static double seed0;
+static double seed1;
+
 /**
- * Generates a random floating point number 
- * between a and b.
- * Source: https://stackoverflow.com/questions/5289613/generate-random-float-between-two-floats
+ * Initialize seeds for pseudo-RNG
  */
-float RandomFloat(float a, float b) {
-    float random = ((float) rand()) / (float) RAND_MAX; // generate random number between 0 and 1
-    float diff = b - a;
-    float r = random * diff;
-    return a + r;
+void init_seeds(void) {
+    seed0 = 0.15924135;
+    seed1 = (double)time(NULL);
+    while (seed1 >=1 ) seed1 /= 10.0; // shift right a decimal place until > 1
+}
+
+/**
+ * Generate a random floating point number between a and b
+ * Algorithm sourced from Prof. Deng's lecture 2 notes
+ */
+double random_float(double a, double b) {
+    double temp = seed0;
+    seed0 = seed1;
+    if (seed1 + temp > 1.0)
+        seed1 = fmod((seed1 + temp) - 1.0, 1.0);
+    else
+        seed1 = fmod((seed1 + temp), 1.0);
+    return a + (seed1 * (b-a));
 }
 
 /**
  * Simulate a disk toss
  */
 int toss_disk(void) {
-    float distance = RandomFloat(0, (float)W/2); // Generate random floating point number between 0 and W/2
+    double distance = random_float(0, (double)W/2); // Generate random floating point number between 0 and W/2
     if (distance < (D/2))
         return 1;
     else
@@ -40,18 +56,19 @@ int toss_disk(void) {
 
 
 int main(void) {
+    init_seeds();
     time_t starting_time = time(NULL);
     #ifdef GRAPH // only save probabilities if they will be graphed at end
-    float* probabilities = malloc(sizeof(float) * SAMPLES+1);
+    double* probabilities = malloc(sizeof(double) * SAMPLES+1);
     #endif
     int sum = 0;
     for (int i = 0; i < SAMPLES; i++) {
         sum += toss_disk();
         #ifdef GRAPH
-        probabilities[i] = (float)sum / (i+1);
+        probabilities[i] = (double)sum / (i+1);
         #endif
     }
-    printf("Final estimate probability: %f (%lu second(s)\n", (float)sum / SAMPLES, time(NULL)-starting_time);
+    printf("Final estimate probability: %f (%lu second(s))\n", (double)sum / SAMPLES, time(NULL)-starting_time);
 
     #ifdef GRAPH
     if (SAMPLES > 100000) exit(EXIT_SUCCESS);
@@ -60,7 +77,7 @@ int main(void) {
     FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
     
     for (int i=0; i < SAMPLES; i++) {
-        fprintf(temp, "%lf %lf \n", (float)i, probabilities[i]); //Write the data to a temporary file
+        fprintf(temp, "%lf %lf \n", (double)i, probabilities[i]); //Write the data to a temporary file
     }
 
     for (int i=0; i < 2; i++) {
