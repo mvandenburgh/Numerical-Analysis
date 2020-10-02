b4af85eef8ba8812245fb2661c4ba72908e46c88
Mike VanDenburgh <mfvandenburg@cs.stonybrook.edu>
2020-10-01 20:47:01 -0400

    implement newton's root finding method in python

--- root_finding.py
+++ root_finding.py
@@ -1,4 +1,4 @@
-# Finds roots of functino f given interval a and b using the bisection method
+# Finds roots of function f given interval a and b using the bisection method
 def bisection(f, a, b, epsilon=0.00000000001, n=10000000000):
     for i in range(n):
         x = (a + b) / 2
@@ -14,3 +14,9 @@ def bisection(f, a, b, epsilon=0.00000000001, n=10000000000):
             print("Bisection method failed...")
             return None
 
+# Finds roots of function f given fPrime (its derivative)
+# and an initial guess x0 using Newton's Method.
+def newton(f, fPrime, x0, iterations=100000):
+    for _ in range(iterations):
+        x0 = x0 - f(x0) / fPrime(x0)
+    return x0
